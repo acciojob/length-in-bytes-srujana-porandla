abec24ed2d6fa29d6d8392c22d1f6c310b3a2760
srujana-porandla <131435996+srujana-porandla@users.noreply.github.com>
2024-06-15 13:05:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const byteSize = (str) => {
   // write your code here
-	function byteSize(str) {
     // Create a new Blob object from the input string
       var blob = new Blob([str]);
 
     // Return the size of the string in bytes
       return blob.size;
-   }
+   
 	
 };
 
